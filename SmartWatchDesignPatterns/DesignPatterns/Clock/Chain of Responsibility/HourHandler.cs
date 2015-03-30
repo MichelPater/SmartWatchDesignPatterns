17aae95e16ed7f6bab1ce5e61665186ac3dead1c
MichelPater <michelpatergit@gmail.com>
2015-03-30 09:59:50 +0200

    Changed the namespace of the Chain of responsibility handlers for beter structure

--- SmartWatchDesignPatterns/DesignPatterns/Clock/Chain of Responsibility/HourHandler.cs
+++ SmartWatchDesignPatterns/DesignPatterns/Clock/Chain of Responsibility/HourHandler.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SmartWatchDesignPatterns.DesignPatterns.Clock
+﻿namespace SmartWatchDesignPatterns.DesignPatterns.Clock.Chain_of_Responsibility
 {
     class HourHandler : TimeHandler
     {
