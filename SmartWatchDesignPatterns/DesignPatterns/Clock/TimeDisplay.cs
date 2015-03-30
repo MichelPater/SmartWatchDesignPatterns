2fc04865933838480d5b7e12ed6dae185959e6b4
MichelPater <michelpatergit@gmail.com>
2015-03-30 10:12:30 +0200

    Added succesor logic to TimeDisplay

--- SmartWatchDesignPatterns/DesignPatterns/Clock/TimeDisplay.cs
+++ SmartWatchDesignPatterns/DesignPatterns/Clock/TimeDisplay.cs
@@ -9,19 +9,20 @@ namespace SmartWatchDesignPatterns.DesignPatterns.Clock
 {
     class TimeDisplay
     {
-        private SecondHandler _secondHandler;
-        private MinuteHandler _minuteHandler;
-        private HourHandler _hourHandler;
+        private SecondHandler _secondHandler = new SecondHandler();
+        private MinuteHandler _minuteHandler = new MinuteHandler();
+        private HourHandler _hourHandler = new HourHandler();
 
         public TimeDisplay()
         {
-            
+            _secondHandler.SetSuccesor(_minuteHandler);
+            _minuteHandler.SetSuccesor(_hourHandler);
         }
 
         public DateTime GetTime()
         {
             DateTime today = DateTime.Today;
-            return new DateTime(today.Year, today.Month, today.Day, _hourHandler.Hour , _minuteHandler.Minute, _secondHandler.Second);
+            return new DateTime(today.Year, today.Month, today.Day, _hourHandler.Hour, _minuteHandler.Minute, _secondHandler.Second);
         }
     }
 }
