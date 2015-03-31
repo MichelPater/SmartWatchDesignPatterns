ed20f31670333ce3a5e995e08c340918c2d0c5c0
MichelPater <michelpatergit@gmail.com>
2015-03-31 16:36:15 +0200

    added TimeCreator and TimeFactory

---
+++ SmartWatchDesignPatterns/DesignPatterns/TimeCreator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SmartWatchDesignPatterns.DesignPatterns
+{
+    class TimeCreator
+    {
+        public Clock.Clock CreateClock()
+        {
+            throw new NotImplementedException("Method is not implemented");
+        }
+
+        public Timer.Timer CreateTimer()
+        {
+            throw new NotImplementedException("Method is not implemented");
+        }
+
+        public Stopwatch.Stopwatch CreateStopwatch()
+        {
+            throw new NotImplementedException("Method is not implemented");
+        }
+    }
+}
