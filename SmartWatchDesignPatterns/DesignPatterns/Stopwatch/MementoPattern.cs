7d9e2270ac074affe8d76db7e249a81c8eb4ea21
Gerjan <gerjan_van_oenen@hotmail.com>
2015-03-31 16:48:59 +0200

    StopWatch Class en MementoPattern

---
+++ SmartWatchDesignPatterns/DesignPatterns/Stopwatch/MementoPattern.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartWatchDesignPatterns.DesignPatterns.Stopwatch
+{
+    class MementoPattern
+    {
+
+    }
+
+    class Originator
+    {
+        private string _savedTime;
+
+        public string savedTime
+        {
+            get { return _savedTime; }
+            set { _savedTime = value; }
+        }
+
+        public Memento CreateMemento()
+        {
+            return (new Memento(_savedTime));
+        }
+
+        public void setMemento(Memento memento)
+        {
+            savedTime = memento.savedTime;
+        }
+
+    }
+
+    class Memento
+    {
+        private string _savedTime;
+
+        public Memento(string savedTime)
+        {
+            this._savedTime = savedTime;
+        }
+
+        public string savedTime
+        {
+            get { return _savedTime; }
+        }
+    }
+
+    class CareTaker
+    {
+        private Memento _memento;
+
+        public Memento Memento
+        {
+            get { return _memento; }
+            set { _memento = value; }
+        }
+    }
+}
