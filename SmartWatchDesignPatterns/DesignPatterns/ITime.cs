0ead62c1bd8515b3f19d3685e1645d84073ad61d
MichelPater <michelpatergit@gmail.com>
2015-03-31 16:57:01 +0200

    ITime and TimeCreator edited

--- SmartWatchDesignPatterns/DesignPatterns/ITime.cs
+++ SmartWatchDesignPatterns/DesignPatterns/ITime.cs
@@ -3,14 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using SmartWatchDesignPatterns.DesignPatterns.Clock;
 
 namespace SmartWatchDesignPatterns.DesignPatterns
 {
-    class TimeFactory
+    interface ITime
     {
-
-
-
+        String getTitle();
     }
 }
